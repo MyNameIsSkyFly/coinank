f82cca95013cbdc8b9eb4e56b5ce75c17f79bf43
gy <agaoyin@163.com>
2023-11-16 22:10:36 +0800

    feat: flutter 和 ios 通信

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,42 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@objc class AppDelegate: FlutterAppDelegate{
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let api = MessageHostApi()
+        SetUpFLTMessageHostApi(controller.binaryMessenger, api)
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
+
+class MessageHostApi: NSObject,FLTMessageHostApi {
+    func toTotalOiWithError(_ error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        print("toTotalOiWithError")
+    }
+    
+    func changeDarkModeIsDark(_ isDark: Bool, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        print("changeDarkModeIsDark")
+        
+    }
+    
+    func changeLanguageLanguageCode(_ languageCode: String, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        print("changeLanguageLanguageCode")
+        
+    }
+    
+    func changeUpColorIsGreenUp(_ isGreenUp: Bool, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        print("changeUpColorIsGreenUp")
+        
+    }
+    
+    
+}
+
