17a59a6655244d30819acab1a456e01bbed0c9e8
gy <agaoyin@163.com>
2023-11-28 15:30:36 +0800

    chore: 修改图表模块样式和web跳转

--- lib/modules/chart/chart_view.dart
+++ lib/modules/chart/chart_view.dart
@@ -1,6 +1,6 @@
+import 'package:ank_app/constants/urls.dart';
 import 'package:ank_app/entity/chart_entity.dart';
 import 'package:ank_app/modules/main/main_logic.dart';
-import 'package:ank_app/pigeon/host_api.g.dart';
 import 'package:ank_app/res/export.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -43,21 +43,24 @@ class ChartPage extends StatelessWidget {
                   : [
                       SliverToBoxAdapter(
                         child: _ChartItem(
-                          title: '热门数据',
+                          iconName: Assets.chartLeftIconHotData,
+                          title: S.current.hotData,
                           data: state.dataMap['hotData'] ?? [],
                         ),
                       ),
                       const SliverToBoxAdapter(child: Gap(20)),
                       SliverToBoxAdapter(
                         child: _ChartItem(
-                          title: 'BTC数据',
+                          iconName: Assets.chartLeftIconBtcData,
+                          title: S.current.btcData,
                           data: state.dataMap['btcData'] ?? [],
                         ),
                       ),
                       const SliverToBoxAdapter(child: Gap(20)),
                       SliverToBoxAdapter(
                         child: _ChartItem(
-                          title: '其他数据',
+                          iconName: Assets.chartLeftIconOtherData,
+                          title: S.current.otherData,
                           data: state.dataMap['otherData'] ?? [],
                         ),
                       ),
@@ -71,10 +74,16 @@ class ChartPage extends StatelessWidget {
 }
 
 class _ChartItem extends StatelessWidget {
-  const _ChartItem({super.key, required this.data, required this.title});
+  const _ChartItem({
+    super.key,
+    required this.data,
+    required this.title,
+    required this.iconName,
+  });
 
   final List<ChartEntity> data;
   final String title;
+  final String iconName;
 
   @override
   Widget build(BuildContext context) {
@@ -82,9 +91,19 @@ class _ChartItem extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(
-          title,
-          style: Styles.tsBody_16m(context),
+        Row(
+          children: [
+            Image.asset(
+              iconName,
+              width: 20,
+              height: 20,
+            ),
+            const Gap(5),
+            Text(
+              title,
+              style: Styles.tsBody_16m(context),
+            ),
+          ],
         ),
         const Gap(15),
         Wrap(
@@ -93,15 +112,15 @@ class _ChartItem extends StatelessWidget {
           children: List.generate(
             data.length,
             (idx) => InkWell(
-              onTap: () => MessageHostApi()
-                  .toChartWeb(data[idx].path ?? '', data[idx].title ?? ''),
+              onTap: () => AppNav.openWebUrl(
+                  url: '${Urls.h5Prefix}${data[idx].path}',
+                  title: data[idx].title),
               child: Container(
                 height: 46,
                 width: AppConst.width / 2 - 20,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                      color: Theme.of(context).dividerTheme.color!, width: 1),
+                  color: Theme.of(context).cardColor,
                 ),
                 alignment: Alignment.center,
                 child: Text(
