f523aefb152efde163b27dd6fcdc8e30d93e1110
elliot <elliotkhd@gmail.com>
2023-11-29 17:00:42 +0800

    feat: 合约持仓页完成；格式化数字时添加"T"单位；修改下拉刷新头

--- lib/util/format_util.dart
+++ lib/util/format_util.dart
@@ -2,49 +2,24 @@ import 'package:decimal/decimal.dart';
 
 class FormatUtil {
   FormatUtil._();
+
   static const double wan = 10000;
   static const double yi = 100000000;
+  static const double trillion = 1000000000000;
   static const String wanUnit = '万';
   static const String yiUnit = '亿';
+  static const String trillionUnit = '万亿';
 
-  static String amountConversion(double amount) {
-    String result = '$amount';
-    double value = 0;
-    double tempValue = 0;
-    double remainder = 0;
-    if ((amount > wan) && (amount < yi)) {
-      tempValue = amount / wan;
-      remainder = amount % wan;
-      if (remainder < (wan / 2)) {
-        value = formatNumber(tempValue, 2);
-      } else {
-        value = formatNumber(tempValue, 2);
-      }
-      if (value == wan) {
-        result = ((value / wan).toStringAsFixed(2) + yiUnit);
-      } else {
-        result = (value.toStringAsFixed(2) + wanUnit);
-      }
+  static String amountConversion(double number) {
+    if (number >= 1000000000000) {
+      return '${(number / 1000000000000).toStringAsFixed(2)}万亿';
+    } else if (number >= 100000000) {
+      return '${(number / 100000000).toStringAsFixed(2)}亿';
+    } else if (number >= 10000) {
+      return '${(number / 10000).toStringAsFixed(2)}万';
     } else {
-      if (amount > yi) {
-        tempValue = amount / yi;
-        remainder = amount % yi;
-        if (remainder < (yi / 2)) {
-          value = formatNumber(tempValue, 2);
-        } else {
-          value = formatNumber(tempValue, 2);
-        }
-        result = (value.toStringAsFixed(2) + yiUnit);
-      } else {
-        result = amount.toStringAsFixed(2);
-      }
+      return number.toString();
     }
-    return result;
-  }
-
-  static double formatNumber(double number, int precision) {
-    final bigDecimal = Decimal.parse('$number');
-    return bigDecimal.round(scale: precision).toDouble();
   }
 
   static void test() {
