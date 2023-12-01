d98a74f9d8f6462e8e9f9e93f8e454c2dfe0e2d0
gy <agaoyin@163.com>
2023-12-01 20:19:39 +0800

    chore: 修改web userAgent

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,7 +1,7 @@
 import 'package:ank_app/util/app_util.dart';
 
 class Urls {
-  static String strDomain = 'tv.coinsoto.com';
+  static String strDomain = 'coinsoto.com';
   static String h5Prefix = 'https://$strDomain';
   static String depthOrderDomain = 'cdn01.coinsoto.com';
   static String uniappDomain =
